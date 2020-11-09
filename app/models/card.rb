20f5068c49c0af1b35278dce922c64f3ae91b8c2
Bee-bird <64572909+Takuma-Abe@users.noreply.github.com>
2020-11-09 20:58:33 +0900

    add create index destroy action

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :user
 
-  validates :card_token, :customer_token, presemce: true
+  validates :card_token, :customer_token, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resources :transactions, only: [:index, :new, :create]
     resources :comments , only: [:create]
   end
-  resources :cards, only: [:index, :new, :create, :destrory]
+  resources :cards, only: [:index, :new, :create, :destroy]
 end
