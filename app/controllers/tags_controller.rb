1c46286c230f26af7e2e4f8b24082519edddc52e
Bee-bird <64572909+Takuma-Abe@users.noreply.github.com>
2020-12-26 15:31:52 +0900

    add where like to search tag tables

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,6 +3,11 @@ class TagsController < ApplicationController
     puts "************"
     puts "タグ: #{params[:tag_name]}"
     puts "************"
-    render json: {tags: ["hoge","fuga","foo","bar"]}
+    render json: {tags: []} if params[:tag_name] == ""
+    tags = Tag.where(['name LIKE ?', "%#{params[:tag_name]}%"])
+    render json: {tags: tags}
   end
 end
+
+# where of "?" is placeholder to put 2nd argument on "?"
+# '%value%' means 'eqtn 0 string', 'value_' means '1 letter' 
