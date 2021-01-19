ca47bccc829206fd608fc7369d397a3a47b13627
Bee-bird <64572909+Takuma-Abe@users.noreply.github.com>
2021-01-19 15:49:12 +0900

    add webpacker_env.rb to load ENV of payjp

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,4 +1,6 @@
 # credentialsの記述に変更（ENV['PAYJP_SECRET_KEY']）
 # Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
-Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = Rails.application.credentials.payjp[:pc_key]
+# Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = Rails.application.credentials.payjp[:pc_key]
 
+require_relative '../config/application'
++require_relative "../webpacker_env"
