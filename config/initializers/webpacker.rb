736a317d93e8da9354e5bed67f3c7b6979c9fb27
Bee-bird <64572909+Takuma-Abe@users.noreply.github.com>
2021-01-19 19:26:16 +0900

    test4

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,5 +1,5 @@
 # credentialsの記述に変更（ENV['PAYJP_SECRET_KEY']）
 # Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
-Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = Rails.application.credentials.payjp[:public_key]
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = "pk_test_836c8c058fc14d56a4aa5c57"
 # require_relative '../application'
 # require_relative '../webpacker_env'
